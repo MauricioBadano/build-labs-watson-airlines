3eb622091d3a35b1fd858117095c6db57a55704d
MauricioBadano <mau@mau.com>
2023-07-15 02:16:31 -0300

    Created main

---
+++ source/back-end/app.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const mongoose = require('mongoose');
+const mongo = require("mongodb");
+
+async function main(){
+
+    // Get global variables from .env file
+    require("dotenv").config({path: path.resolve(__dirname,".env")});
+
+    // Connect to database
+    const { create_connection } = require("./mongodb.js");
+    await create_connection();    
+}
+
+main();
